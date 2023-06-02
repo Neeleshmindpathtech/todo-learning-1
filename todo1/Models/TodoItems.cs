b8778735057c798c0fe1a56ee470f96e53d315c9
neelesh <neelesh@mindpathtech.com>
2023-06-02 14:38:39 +0530

    todo learnig 1 time c#

---
+++ todo1/Models/TodoItems.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace todo1.Models
+
+{
+    public class TodoItems
+    {
+        public long Id { get ; set;}
+        public string? Name { get; set;}
+
+        public bool isComplete { get; set;}
+        public string? Secret { get; set;}
+
+    }
+}
